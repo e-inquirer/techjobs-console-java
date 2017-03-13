1e33a5fdddade62ac8e5a05dfd28394bf6dcb5cf
e-inquirer <sorour.a@gmail.com>
2017-03-13 15:05:52 -0400

    Implemented `pringJobs()` method; output is still unformatted & raw.

--- src/org/launchcode/techjobs/console/TechJobs.java
+++ src/org/launchcode/techjobs/console/TechJobs.java
@@ -2,6 +2,7 @@ package org.launchcode.techjobs.console;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 /**
@@ -111,6 +112,11 @@ public class TechJobs {
     // Print a list of jobs
     private static void printJobs(ArrayList<HashMap<String, String>> someJobs) {
 
-        System.out.println("printJobs is not implemented yet");
+        for (HashMap<String, String> jobData : someJobs) {
+//            for (Map.Entry<String, String> jobData : someJobs {
+//            System.out.println(student.getKey() + " (" + student.getValue() + ")");
+            System.out.println(jobData);
+        }
+//        System.out.println("\n*****\n" + "<printJobs-TEXT-STUB" + "\n*****\n");
     }
 }
