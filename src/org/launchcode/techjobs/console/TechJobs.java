9b02e4b09aa53d191e4eebddfd5fbabcc67d39c7
e-inquirer <sorour.a@gmail.com>
2017-03-16 15:37:45 -0400

    updated `printJobs()`; replaced `someJobs.size() == 0` binary with `someJobs.isEmpty()`

--- src/org/launchcode/techjobs/console/TechJobs.java
+++ src/org/launchcode/techjobs/console/TechJobs.java
@@ -112,7 +112,7 @@ public class TechJobs {
     // Print a list of jobs
     private static void printJobs(ArrayList<HashMap<String, String>> someJobs) {
 
-        if (someJobs.size() == 0) {
+        if (someJobs.isEmpty()) {
             System.out.println("\nSorry, your search did not produce any results.");
         } else {
 
